df45dd78baaa18c8b0790f196cb28702d9ad733b
Дмитрий Мурзин <dmitriymyrzin0908@gmail.com>
2023-09-18 13:42:19 +0300

    Подключил бд

--- AvaloniaApplication2/ViewModels/MainWindowViewModel.cs
+++ AvaloniaApplication2/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using AvaloniaApplication2.Service;
+using System.Linq;
 
 namespace AvaloniaApplication2.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public MainWindowViewModel(Database db)
+        public MainWindowViewModel()
         {
-            List = new TodoListViewModel(db.GetItems());
+            List = new TodoListViewModel();
         }
         public TodoListViewModel List { get; }
     }
